e84b579da79260d33ff4d3b4c713eb744eb30ef1
ambit741235 <76868481+ambit741235@users.noreply.github.com>
2021-01-07 12:37:38 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -75,7 +75,7 @@ function draw() {
   drawSprites();
   textSize(20);
   fill(255);
-  text("Distance: "+ distance,350,30);
+  text("Distance: "+ distance,/*350,30*/width/2,30);
   
   if(gameState===PLAY){
     distance = distance + Math.round(getFrameRate()/50);
